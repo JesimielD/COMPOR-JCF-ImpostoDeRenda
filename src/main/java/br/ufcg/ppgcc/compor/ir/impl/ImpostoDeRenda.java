7776a044ce3acc74e191546d87a5412b9eba601f
JesimielD <jesimiel01@hotmail.com>
2014-08-23 15:10:55 -0300

    Tarefa T.02.01 finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -1,15 +1,21 @@
 package br.ufcg.ppgcc.compor.ir.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
 	private List<Titular> titular1 = new ArrayList<Titular>() ;
+	
+	private Map<Titular, List<FontePagadora>> mapaFontes = new HashMap<Titular, List<FontePagadora>>();
+	private List<FontePagadora> fontes = new ArrayList<FontePagadora>() ;
 	
 	public void criarNovoTitular(Titular titular) {
 		if (titular.getNome() == null){
@@ -33,6 +39,17 @@ public class ImpostoDeRenda implements FachadaExperimento {
 		return titular1;
 
 		//return null;
+	}
+
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
+		
+		fontes.add(fonte);
+		mapaFontes.put(titular, fontes);
+		
+	}
+
+	public List<FontePagadora> listarFontes(Titular titular) {
+		return mapaFontes.get(titular);
 	}
 
 }
