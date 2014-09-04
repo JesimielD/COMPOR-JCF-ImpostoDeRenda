5103889245cd95b31087125037676f825f595c73
jesimiel <jesimiel@192.168.25.12>
2014-09-03 23:09:59 -0300

    T.03.04

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -13,100 +13,108 @@ import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
-	private List<Titular> titular1 = new ArrayList<Titular>() ;
+	private List<Titular> titular1 = new ArrayList<Titular>();
 	private Map<Titular, List<FontePagadora>> mapaFontes = new HashMap<Titular, List<FontePagadora>>();
-	private List<FontePagadora> fontes = new ArrayList<FontePagadora>() ;
-	
-	private Map<Titular, List<Dependente>> mapaDependentes = new HashMap<Titular, List<Dependente>>();
-	private List<Dependente> dependentes = new ArrayList<Dependente>() ;
-	
-	
-	public void criarNovoTitular(Titular titular) {
-		if (titular.getNome() == null){
-			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");				
-		} 
+	private List<FontePagadora> fontes = new ArrayList<FontePagadora>();
 
-		if (titular.getCpf() == null){
-			throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");				
+	private Map<Titular, List<Dependente>> mapaDependentes = new HashMap<Titular, List<Dependente>>();
+	private List<Dependente> dependentes = new ArrayList<Dependente>();
+
+	public void criarNovoTitular(Titular titular) {
+		if (titular.getNome() == null) {
+			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
 		}
-		if (titular.getCpf().length() !=14){
-			throw new ExcecaoImpostoDeRenda("O campo CPF está inválido");				
+
+		if (titular.getCpf() == null) {
+			throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
 		}
-		
+		if (titular.getCpf().length() != 14) {
+			throw new ExcecaoImpostoDeRenda("O campo CPF está inválido");
+		}
+
 		fontes = new ArrayList<FontePagadora>();
 		titular1.add(titular);
 		mapaFontes.put(titular, fontes);
-		
 
-		
 	}
 
 	public List<Titular> listarTitulares() {
-	
+
 		return titular1;
 
 	}
+
 	public List<FontePagadora> listarFontes(Titular titular) {
 		// TODO Auto-generated method stub
 		return mapaFontes.get(titular);
 	}
+
 	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
 		/* Primeira coisa, eu preciso pegar a fonte do titular, se já existir */
 		List<FontePagadora> fontesDoTitular = mapaFontes.get(titular);
-		
-		if (fonte.getNome() == null){
-			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");				
-		} 
 
-		if (fonte.getCpfCnpj() == null){
-			throw new ExcecaoImpostoDeRenda("O campo CPF/CNPJ é obrigatório");				
+		if (fonte.getNome() == null) {
+			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
 		}
-		if (fonte.getRendimentoRecebidos() == 0){
-			throw new ExcecaoImpostoDeRenda("O campo rendimentos recebidos é obrigatório");				
+
+		if (fonte.getCpfCnpj() == null) {
+			throw new ExcecaoImpostoDeRenda("O campo CPF/CNPJ é obrigatório");
 		}
-		if (fonte.getRendimentoRecebidos() < 0){
-			throw new ExcecaoImpostoDeRenda("O campo rendimentos recebidos deve ser maior que zero");				
+		if (fonte.getRendimentoRecebidos() == 0) {
+			throw new ExcecaoImpostoDeRenda(
+					"O campo rendimentos recebidos é obrigatório");
 		}
-		if ((fonte.getCpfCnpj() != null) && (fonte.getCpfCnpj().length() != 18)){
-			throw new ExcecaoImpostoDeRenda("O campo CPF/CNPJ é inválido");				
+		if (fonte.getRendimentoRecebidos() < 0) {
+			throw new ExcecaoImpostoDeRenda(
+					"O campo rendimentos recebidos deve ser maior que zero");
 		}
-		
-		//*/
+		//if ((fonte.getCpfCnpj() != null) && 
+				
+				if (fonte.getCpfCnpj().length() != 18) {
+			throw new ExcecaoImpostoDeRenda("O campo CPF/CNPJ é inválido");
+		}
+
+		// */
 		/* Verifico se existe, ou se é null */
 		if (fontesDoTitular == null) {
 			throw new ExcecaoImpostoDeRenda("Titular não cadastrado");
 			/* Se não existe, cria nova lista */
-			
+
 		}
 		/* Adiciona a nova fonte na nova lista, ou na lista que já existia */
-		
+
 		fontesDoTitular.add(fonte);
-	
-		
+
 	}
+
+	
 
 	public void criarDependente(Titular titular, Dependente dependente) {
-		/*pego os dependentes*/
-		List<Dependente> dependentesDoTitular = mapaDependentes.get(titular);
-	
-		dependentes.add(dependente);	
-		mapaDependentes.put(titular, dependentes);
-		
-		if (dependentesDoTitular == null) {
-		
-			dependentesDoTitular = new ArrayList<Dependente>();
-	
+		if (mapaDependentes.containsKey(titular) == false) {
+			mapaDependentes.put(titular, new ArrayList<Dependente>());
 		}
-		
-		dependentesDoTitular.add(dependente);
-		mapaDependentes.put(titular,dependentesDoTitular);
-		dependentes.add(dependente);
-		
-		
+		if (dependente.getNome() == null)
+			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
+		else if (dependente.getCpf() == null)
+			throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
+		else if (dependente.getTipo() == 0)
+			throw new ExcecaoImpostoDeRenda("O campo tipo é obrigatório");
+		else if (dependente.getTipo() < 0)
+			throw new ExcecaoImpostoDeRenda("O campo tipo é inválido");
+		else if(dependente.getCpf().length()!= 14)  
+				throw new ExcecaoImpostoDeRenda("O campo CPF é inválido"); 
+		else if (titular1.contains(titular) == false)
+			throw new ExcecaoImpostoDeRenda("Titular não cadastrado");
+		else
+			mapaDependentes.get(titular).add(dependente);
 	}
+	
+	
+		
+
 
 	public List<Dependente> listarDependentes(Titular titular) {
-		 return mapaDependentes.get(titular);
+		return mapaDependentes.get(titular);
 	}
 
 }
